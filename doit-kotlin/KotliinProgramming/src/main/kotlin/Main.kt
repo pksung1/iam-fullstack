50818bc561da4317bd8116855d1db88b86ccc405
seonpark <pksung1@gmail.com>
2022-01-25 19:48:40 +0900

    feat : Learn kotlin

---
+++ doit-kotlin/KotliinProgramming/src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+import kotlin.math.PI
+import com.example.edu.Person as EduPerson
+import com.example.other.Person as OtherPerson
+fun main(args: Array<String>) {
+    val intro: String = "안녕하세요"
+    val num: Int = 10
+
+    var a: String? = null
+
+    val eduUser = EduPerson("Edu Person", age = 26)
+    val otherUser = OtherPerson("Other Person", age = 22)
+
+    println("intro $intro, num: $num")
+    println("eduUser ${eduUser.name} ${eduUser.age}")
+    println("otherUser ${otherUser.name} ${otherUser.age}")
+
+    a = "aaa"
+    println("a is $a, length: ${a?.length ?: -1}");
+    println(PI)
+    val b = 10
+    println(b.javaClass)
+}
